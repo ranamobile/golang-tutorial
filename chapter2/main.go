b3d039adf1174a78385199deb5f0c57cb03c0bb0
Ariana Hong <aunkei.hong@gmail.com>
2019-09-27 15:08:24 -0400

    Add boolean examples

--- chapter2/main.go
+++ chapter2/main.go
@@ -3,10 +3,19 @@ package main
 import "fmt"
 
 func main() {
+	// Number examples
 	fmt.Println("1 + 1 =", 1 + 1)
 	fmt.Println("1.0 + 1.0 =", 1.0 + 1.0)
 
+	// String examples
 	fmt.Println(len("Hello, World"))
 	fmt.Println("Hello, World"[1])
 	fmt.Println("Hello, " + "World")
+
+	// Boolean examples
+	fmt.Println(true && true)
+	fmt.Println(true && false)
+	fmt.Println(true || true)
+	fmt.Println(true || false)
+	fmt.Println(!true)
 }
